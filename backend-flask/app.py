dbcfa39c66b6a018c8d31a470bbd9efc9449a890
nullreturn <shodannull@protonmail.com>
2023-02-27 17:39:29 +0000

    xray changes - backend app,yaml compose

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -14,6 +14,10 @@ from services.messages import *
 from services.create_message import *
 from services.show_activity import *
 
+#AWS XRAY
+from aws_xray_sdk.core import xray_recorder
+from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
+
 # app.py updates
     
 from opentelemetry import trace
@@ -36,7 +40,10 @@ app = Flask(__name__)
 FlaskInstrumentor().instrument_app(app)
 RequestsInstrumentor().instrument()
 
-
+# XRAY
+xray_url = os.getenv("AWS_XRAY_URL")
+xray_recorder.configure(service='Cruddur', dynamic_naming=xray_url)
+XRayMiddleware(app, xray_recorder)
 
 
 #####################################
