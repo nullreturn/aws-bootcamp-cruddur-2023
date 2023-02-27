e753fa132bb4d05070b661779d4d32bf4588b29a
nullreturn <shodannull@protonmail.com>
2023-02-27 21:09:00 +0000

    correced backend.app

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -34,20 +34,20 @@ provider.add_span_processor(processor)
 trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
 
+# X-RAY ----------
+xray_url = os.getenv("AWS_XRAY_URL")
+xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
+
+################# APP FLASK #########################
 app = Flask(__name__)
 
 #XRAY
+xray_recorder.configure(service='Cruddur', dynamic_naming=xray_url)
 XRayMiddleware(app, xray_recorder)
 
 # Initialize automatic instrumentation with Flask
 FlaskInstrumentor().instrument_app(app)
 RequestsInstrumentor().instrument()
-
-# XRAY
-xray_url = os.getenv("AWS_XRAY_URL")
-xray_recorder.configure(service='Cruddur', dynamic_naming=xray_url)
-XRayMiddleware(app, xray_recorder)
-
 
 #####################################
 frontend = os.getenv('FRONTEND_URL')
