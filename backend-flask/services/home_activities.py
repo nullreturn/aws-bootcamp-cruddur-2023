0bb8542e63c15fc88341783f9143d947ed1fb3cf
nullreturn <shodannull@protonmail.com>
2023-03-03 21:23:24 +0000

    tab typos in homeactivites.py

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -1,19 +1,24 @@
 from datetime import datetime, timedelta, timezone
 from opentelemetry import trace
 #import logging
-#tracer = trace.get_tracer("home.activites")
+
+tracer = trace.get_tracer("home.activities")
+
 class HomeActivities:
   def run():
-    now = datetime.now(timezone.utc).astimezone()
+    with tracer.start_as_current_span("home-activites-mock-data"):
     #logger.info("HomeActivities")
     #with tracer.start_as_current_span("home-activites-mock-data"):    
     #span = trace.get_current_span()
     #now = datetime.now(timezone.utc).astimezone()
     #span.set_attribute("app.now", now.isoformat())
-    results = [{
+      span = trace.get_current_span()
+      now = datetime.now(timezone.utc).astimezone()
+      span.set_attribute("app.now", now.isoformat())
+      results = [{
         'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
         'handle':  'Andrew Brown',
-        'message': 'Cloud is fun!',
+        'message': 'Cloud is very fun!',
         'created_at': (now - timedelta(days=2)).isoformat(),
         'expires_at': (now + timedelta(days=5)).isoformat(),
         'likes_count': 5,
@@ -41,15 +46,15 @@ class HomeActivities:
       },
       {
         'uuid': '248959df-3079-4947-b847-9e0892d1bab4',
-        'handle':  'Garek',
-        'message': 'My dear doctor, I am just simple tailor',
+        'handle':  'Mr Harry Fordo',
+        'message': 'Youre a Wizard Jerry',
         'created_at': (now - timedelta(hours=1)).isoformat(),
         'expires_at': (now + timedelta(hours=12)).isoformat(),
         'likes': 0,
         'replies': []
       }
       ]
-      #span.set_attribute("app.result_length", len(results))
-    return results
-    #results
-    
+      span.set_attribute("app.result_length", len(results))
+      #span.set_attribute("user.id", len(results))
+      return results
+Footer
