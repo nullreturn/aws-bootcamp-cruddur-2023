daa8ff89804ceb73faca68e1d9c02882ff90c99f
nullreturn <shodannull@protonmail.com>
2023-03-07 19:26:21 +0000

    comments backend app.py

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -171,6 +171,26 @@ def data_home():
   data = HomeActivities.run()
   return data, 200
 
+
+#@app.route("/api/activities/home", methods=['GET'])
+#@xray_recorder.capture('activities_home')
+#def data_home():
+#  access_token = extract_access_token(request.headers)
+ # try:
+#    claims = cognito_jwt_token.verify(access_token)
+#    # authenicatied request
+#    app.logger.debug("authenicated")
+#    app.logger.debug(claims)
+#    app.logger.debug(claims['username'])
+#    data = HomeActivities.run(cognito_user_id=claims['username'])
+#  except TokenVerifyError as e:
+    # unauthenicatied request
+#    app.logger.debug(e)
+#    app.logger.debug("unauthenicated")
+#    data = HomeActivities.run()
+#  return data, 200
+
+
 @app.route("/api/activities/notifications", methods=['GET'])
 def data_notifications():
   data = NotificationsActivities.run()
